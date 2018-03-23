ded2df02f645aadb31d9e96ad4415fa50a8707e8
Ben Staley <bs16aat@herts.ac.uk>
2018-03-23 18:49:01 +0000

    Add project files.

---
+++ TheUnitGallery/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TheUnitGallery.Startup))]
+namespace TheUnitGallery
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
