e5d9fe760d2888baded9ff2bd062505f949fcaaa
Ben Staley <bs16aat@herts.ac.uk>
2018-03-23 19:14:03 +0000

    Basic Homepage Dropdown Menu Created

--- TheUnitGallery/App_Start/BundleConfig.cs
+++ TheUnitGallery/App_Start/BundleConfig.cs
@@ -26,6 +26,9 @@ namespace TheUnitGallery
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/dropdowns").Include(
+            "~/Scripts/header.dropdown.js"));
         }
     }
 }
