8a9ca581d72cde29214881be3d0f4709a76a6456
Ben Staley <bs16aat@herts.ac.uk>
2018-03-23 19:34:31 +0000

    Added IMS Dashboard

---
+++ TheUnitGallery/Controllers/LoginController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TheUnitGallery.Controllers
+{
+    [Authorize]
+    public class LoginController : Controller
+    {
+        // GET: 
+        [AllowAnonymous]
+        public ActionResult _LoginPartial(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return PartialView();
+        }
+    }
+}
